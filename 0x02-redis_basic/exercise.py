731f23476e26e3137ecfc761349dccf9b07db213
Walaakhkh <Walaakhalil98@gmail.com>
2024-10-23 21:59:55 +0000

    task 4

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -48,7 +48,7 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
+def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
         """ Get data from Redis and optionally transform it """
         data = self._redis.get(key)
         if fn:
